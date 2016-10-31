3fffd4d594b8e9a6ce33c8404059592151f228c8
Programmist <717720@list.ru>
2016-10-31 18:03:06 +0900

    commit

--- tests/CinemaTests.js
+++ tests/CinemaTests.js
@@ -14,52 +14,61 @@ suite("cinema tests", function() {
             });
         });
 
-        test("customer ask one ticket", function() {
-            let count = 1;
-            let tickets = cinema.askTickets(count);
-            assert.equal(tickets.length, 1);
+        suite("parameter film is not important", function() {
+            test("customer ask one ticket", function() {
+                let count = 1;
+                let tickets = cinema.askTickets(count);
+                assert.equal(tickets.length, 1);
+            });
+
+            test("customer ask 5 tickets", function() {
+                let count = 5;
+                let tickets = cinema.askTickets(count);
+                assert.equal(tickets.length, 5);
+            });
         });
 
-        test("customer ask 5 tickets", function() {
-            let count = 5;
-            let tickets = cinema.askTickets(count);
-            assert.equal(tickets.length, 5);
+        suite("available films", function() {
+            test("customer ask tickets to Iron Man", function() {
+                let count = 3;
+                let film = "Iron Man";
+                let tickets = cinema.askTickets(count, film);
+
+                for (let i in tickets) {
+                    assert.equal(tickets[i].film, film);
+                }
+            });
+
+            test("customer ask tickets to Interstellar", function() {
+                let count = 2;
+                let film = "Interstellar";
+                let tickets = cinema.askTickets(count, film);
+
+                for (let i in tickets) {
+                    assert.equal(tickets[i].film, film);
+                }
+            });
+
+            suite("required count more cinema capacity", function() {
+                test("customer ask 300 tickets - more then cinema capacity", function() {
+
+                    let count = 300;
+                    let film = "Iron Man";
+                    let tickets = cinema.askTickets(count, film);
+                    assert.equal(tickets.length, cinema.capacity);
+
+                });
+            });
+
         });
 
-        test("customer ask tickets to Iron Man", function() {
-            let count = 3;
-            let film = "Iron Man";
-            let tickets = cinema.askTickets(count, film);
-
-            for (let i in tickets) {
-                assert.equal(tickets[i].film, film);
-            }
-        });
-
-        test("customer ask tickets to Interstellar", function() {
-            let count = 2;
-            let film = "Interstellar";
-            let tickets = cinema.askTickets(count, film);
-
-            for (let i in tickets) {
-                assert.equal(tickets[i].film, film);
-            }
-        });
-
-        test("customer ask tickets to unavailable film Snowden", function() {
-            let count = 5;
-            let film = "Snowden";
-            let tickets = cinema.askTickets(count, film);
-            assert.equal(tickets.length, 0);
-        });
-
-        test("customer ask 300 tickets - more then cinema capacity", function() {
-
-            let count = 300;
-            let film = "Iron Man";
-            let tickets = cinema.askTickets(count, film);
-            assert.equal(tickets.length, cinema.capacity);
-
+        suite("unavailable films", function() {
+            test("customer ask tickets to unavailable film Snowden", function() {
+                let count = 5;
+                let film = "Snowden";
+                let tickets = cinema.askTickets(count, film);
+                assert.equal(tickets.length, 0);
+            });
         });
 
         teardown(function(){
